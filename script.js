d8512fe50262ffd13696e8a3da6f949d669f714f
Davvead <davead461@gmail.com>
2022-03-14 18:42:56 +0000

    js

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-document.getElementById('top-prod').innerHTML = `<h1>808's and heartbreaks</h1>`;
-document.getElementById('top-prod').style.marginTop = '1rem';
+// let prodHead = document.querySelector('#prod-heading');
+// console.log(prodHead);
+
+// prodHead.innerText = 'Dave is Great';
+// prodHead.style.color = '#6C757D';
+
+let products = document.getElementsByClassName('prod');
+
+for(let i = 0; i < products.length; i++){
+    if((i + 1) % 2 == 0){
+    products[i].style.background = "#000";
+    }
+};
+console.log(products);
+
+products = Array.from(products);
+
+products.reverse();
